dd8df3d7d16da49b8cf5b5e52079ae075c082a91
CoderFlashier <141394459+CoderFlashier@users.noreply.github.com>
2023-10-23 23:58:31 +0800

    20231023

---
+++ test20231023/test.py
@@ -0,0 +1,23 @@
+""" 你好 世界 """
+
+# print("hello world!")
+# print("hello")
+# print("hello world!")
+
+
+# num1 = 30
+# print("num1:", num1)
+# num1 = num1 - 10
+# print("num1:", num1, "减去了10")
+
+# print(type(1.26))
+# print(type(1))
+# print(type("wdnmd"))
+
+# num1 = 123
+# print(num1)
+# print(type(float(num1)),num1)
+# print(type(str(num1)),num1)
+
+# num1 = 12.34
+# print(num1,int(num1))
